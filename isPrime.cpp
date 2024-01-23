15644ea2dc35119c404895b2dc64e2d234e21346
12345rams <vijay90054442298@gmail.com>
2024-01-24 00:29:16 +0530

    isPrime

---
+++ isPrime.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrime(long long x)
+{
+    if (x < 2)
+        return false;
+    if (x == 2)
+        return true;
+    if (x % 2 == 0)
+        return false;
+
+    for (long long i = 3; i * i <= x; i += 2)
+    {
+        if (x % i == 0)
+            return false;
+    }
+    return true;
+}
+
+
+
